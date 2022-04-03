f530a166f94983b89866c6419433afeca73c049c
michalm7 <mroczekmichal57@gmail.com>
2022-04-03 12:28:10 +0200

    adding new functionality 03.04.2022

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/products', [ProductController::class, 'index']);
 // Route::post('/products', [ProductController::class, 'store']);
 Route::resource('products', ProductController::class);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
